5e8057be9a25db807b7fa6cea1307e49ae469f46
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-06 16:45:43 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,11 @@
 var playlist = {
-  artist: 'song'
+  Childish Gambino: 'This is America',
+}
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+  return playlist;
 }
